9f3063c0add5848427de831597d1e67a700d652c
esteban11519 <eladinof@unal.edu.co>
2020-11-15 14:31:32 -0500

    Funciona, implementar controlador

--- Unity/Cruise control/Assets/CarController.cs
+++ Unity/Cruise control/Assets/CarController.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿/*
+Comentarios generales
+-Se realizan una serie de cálculos para determinar la posición de los carros en función de la inclinación.
+-Se tuvieron que fijar los pivotes del suelo (ground) y de los carritos para que la rotación se diera en cero.
+-El número de pixeles por unidad deben ajustarse para que los cuadros elegidos se ajusten a nuestra imagen de fondo (background)
+-Con el background de 700x500, el ground por 780x457 que contiene 13 unidades (0 a 100 km/h y el margen), además el sistema está 
+coordenado como Dominio=[-14,14] y Rango de [-10,10] cuadros ajustándose perfectamente a las dimensiones del background. La conversión
+de velocidad en km/h a cuadros es:
+
+Y= 6X/25-12
+
+Donde,
+
+Y: Cuadros
+X= Velocidad en km/h
+
+Recordar, que la rotación se da en el pivote coordenado como [0,0].
+
+Hay cosas que solo se pueden hacer con la ayuda de Dios y esta no es la excepción.
+
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Se crea un objeto que va a ser el carro de referencia
-    GameObject carroReferencia;
+    // Objetos 
+        // Carro de referencia
+        GameObject carroReferencia;
+        // El piso.
+        GameObject ground;
 
-    // Según el número de cuadros que haya establecido en X y Y, Según también esté mi ubicación del objeto.
+    // El margen
     public float PADDING=1f;
     
-    // Establecimiento de límites
+    /* 
+    Establecimiento de límites, posición está dado por cuadros en la interfaz de Unity, no por pixeles.
+    */
     float MAXIMO_X=14; 
     float MINIMO_X=-14;
     float MAXIMO_Y=10; 
@@ -18,8 +45,12 @@ public class CarController : MonoBehaviour
     
     // Datos nominales
 
-    public float vN=0;
-    
+    public float vN=30f; // Velocidad en km/h
+        // Rotación
+        public float theta=0f; // Ángulo de rotación en grados.
+        float auxTheta=0f;
+        public bool rotate=false;
+
     // Tiempo de muestreo
 
     float Ts=1f; // Tiempo de muestreo en segundos
@@ -27,56 +58,79 @@ public class CarController : MonoBehaviour
     
         // Datos de prueba
         int i;
-        float sign; 
-    
+
     // Tiempo primario de ejecución
     float last_time;
     
     // En esta parte se inicializan los datos.
     void Start()
     {
-        // Se instancia el carro de referencia
+        // Se instancia el carro de referencia y el ground
         carroReferencia= GameObject.Find("Opacity");
-
+        ground= GameObject.Find("ground");
         // Se fija la posición de referencia
-        carroReferencia.transform.position=new Vector3(vN,0,0);
+        carroReferencia.transform.position=new Vector3((6f*vN/25f -12f),0,carroReferencia.transform.position.z);
 
         // Inicialización de variables de tiempo de muestreo
         Tolerancia_Ts=Ts*0.05f; //Tolerancia de tiempo de muestreo del 5%
         // Se registra el tiempo inicial
         last_time=Time.time;
         i=0;
-        sign=1;
         
     }
 
     // Actualiza los frames periódicamente
     void Update()
     {
+        // Prueba de tiempo: Cada 0.02 segundos se actualiza
+        //Debug.Log(Time.time-last_time);
         // Solo se ejecuta en el tiempo de muestreo
         if((Time.time-last_time)>=(Ts-Tolerancia_Ts))
         {   
             last_time=Time.time;
+            // tiempo de ejecución
             Debug.Log(Time.time);
             i++;// Solo para fines de prueba y se utiliza en la función car_velocity()
-            this.transform.position=new Vector3(sign*car_velocity(),0,0);
+            /*  
+                car_velocity() está en [m/s]. Vector3() tiene tres argumentos: Vector3(posx,posy,posz).
+            */
+            this.transform.position=new Vector3(Mathf.Cos(auxTheta*Mathf.PI/180)*(108*car_velocity()/125-12)
+            ,Mathf.Sin(auxTheta*Mathf.PI/180)*(108*car_velocity()/125-12),transform.position.z);
             // Se fijan los límites de movimiento
             float newX=Mathf.Clamp(transform.position.x,MINIMO_X+PADDING,MAXIMO_X-PADDING);
             float newY=Mathf.Clamp(transform.position.y,MINIMO_Y+PADDING,MAXIMO_Y-PADDING);
+            // transform.position.z estrae la posición que tengo en transform
             this.transform.position=new Vector3(newX,newY,transform.position.z);
+            
+
+            // Se establece el ángulo de rotación
+            if(rotate)
+            {
+                auxTheta=theta;
+                /* 
+                Se hace la rotación del objeto ground. Dado que los sprites Opacity y Car están dentro de ground, cuando se gira ground 
+                se giran ambos.
+                */  
+                ground.transform.Rotate(new Vector3(0,0,auxTheta));
+                // Se adapta la posición del carrito de referencia, según el ángulo y la velocidad donde vN está en km/h.
+                carroReferencia.transform.position=new Vector3(Mathf.Cos(auxTheta*Mathf.PI/180)*(6*vN/25-12)
+                ,Mathf.Sin(auxTheta*Mathf.PI/180)*(6*vN/25-12),carroReferencia.transform.position.z);
+                // Solo se hace la rotación una vez
+                rotate=false;
+            }
+        
         
         }   
     }
 
-    // Función que determina la velocidad
+    // Función que determina la velocidad (Colocar en m/s)
     private float car_velocity()
     {
-        if(i%14==0){
-            sign=-sign;
+        if(i%11==0){
             i=0;
             }   
 
-        return i;
+        return (float)i*10f/3.6f;
     }
     
 }
